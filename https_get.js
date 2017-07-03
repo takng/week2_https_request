713fec7550e659259c0baacb7eabc4396f1d822d
TakNg <tak_ng@yahoo.com>
2017-07-03 15:13:48 +0000

    https

---
+++ https_get.js
@@ -0,0 +1,50 @@
+// while https is built-in to Node, it is a module, so it must be required
+var https = require('https');                               
+
+// the host can be thought of as the domain name you want to read from,
+// and the path is the resource - '/' is the root path, but if you wanted to read a
+// particular resource (like '/login/index.html'), that would be defined in the path
+
+function getAndPrintHTMLChunks () {
+
+    console.log('HERE .');
+
+  var new_data = "";
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+
+// notice that https.get takes a callback with one parameter -
+// response, which is a Stream that represents the HTTP response
+https.get(requestOptions, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data.length);
+
+    console.log(typeof data);
+    new_data = data + "\n";
+    console.log(new_data);
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+});
+
+}
+
+console.log(getAndPrintHTMLChunks());
+
+
+
