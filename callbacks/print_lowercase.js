9e3238f31dd54b14ba87bdfefb2281c85af3ce0c
TakNg <tak_ng@yahoo.com>
2017-07-03 16:54:31 +0000

    https

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,21 @@
+// while https is built-in to Node, it is a module, so it must be required
+
+//var https = require('https');                               
+
+// the host can be thought of as the domain name you want to read from,
+// and the path is the resource - '/' is the root path, but if you wanted to read a
+// particular resource (like '/login/index.html'), that would be defined in the path
+
+function printLowerCase (html) {
+  console.log(html.toLowerCase());
+}
+
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+getHTML(requestOptions, printLowerCase);
+
